564032cbdb591a2dd2f3011afdd9d4c3f88b29cb
Przemysław Lib <przemyslaw.lib@wp.pl>
2012-02-26 21:54:33 +0100

    Added model subject which will group similar instructions

--- tswproject/ins/models.py
+++ tswproject/ins/models.py
@@ -4,9 +4,16 @@ from pygments.lexers import get_all_lexers
 ALL_LANGUAGES = ((item[1][0],item[0]) for item in get_all_lexers())
 
 # Create your models here.
+class Subject(models.Model):
+    title = models.CharField(max_length=250)
+    description = models.TextField()
+    def __unicode__(self):
+        return title
+
 class Instruction(models.Model):
     title = models.CharField(max_length=250)
     introduction = models.TextField()
+    subject = models.ForeignKey(Subject)
     
     def __unicode__(self):
         return self.title
