805606a03248a60a85c4075b166fbc2813253d09
Przemysław Lib <przemyslaw.lib@wp.pl>
2012-02-26 17:53:25 +0100

    Refined admin interface for task and inst, showing snippets and tasks acordingly
    Now Instruction admin site show inlined Tasks, and 3 empty for use. Task show one empty snippet. User can add more via button, or by saving object.

--- tswproject/ins/admin.py
+++ tswproject/ins/admin.py
@@ -1,6 +1,22 @@
 from django.contrib import admin
 from ins.models import Snippet, Task, Instruction
 
+class TaskInline(admin.TabularInline):
+    model = Task
+    extra = 3
+
+class InstAdmin(admin.ModelAdmin):
+    inlines = [TaskInline]
+
+class SnippetInline(admin.TabularInline):
+    model = Snippet
+    extra = 1
+
+class TaskAdmin(admin.ModelAdmin):
+    inlines = [SnippetInline]
+
 admin.site.register(Snippet)
-admin.site.register(Task)
-admin.site.register(Instruction)
+admin.site.register(Task, TaskAdmin)
+admin.site.register(Instruction, InstAdmin)
+
+
