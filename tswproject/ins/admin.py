9c387015ba746e2dcb219ac0d4ce722e3c18cf4b
Przemysław Lib <przemyslaw.lib@wp.pl>
2012-02-26 17:26:23 +0100

    Added task, and instruction classes to admin interface

--- tswproject/ins/admin.py
+++ tswproject/ins/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from ins.models import Snippet
+from ins.models import Snippet, Task, Instruction
 
 admin.site.register(Snippet)
+admin.site.register(Task)
+admin.site.register(Instruction)
