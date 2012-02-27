b113c256457c9b268aaac6f8b848a8d1988ba8c9
Przemysław Lib <przemyslaw.lib@wp.pl>
2012-02-27 11:23:29 +0100

    Added restrictions

--- tswproject/ins/views.py
+++ tswproject/ins/views.py
@@ -3,24 +3,26 @@ from django.template import RequestContext
 from django.contrib.auth import login, logout, authenticate
 from django.contrib import messages
 from ins.models import Subject, Instruction, Task, Snippet
+from guardian.decorators import permission_required_or_403
 # Create your views here.
 def home(request):
     c = RequestContext(request)
-    subject_list = Subject.objects.all()[:5]
+    subject_list = Subject.objects.all()
     return render_to_response('ins/index.html', {'subject_list': subject_list}, c)
 
 def subject(request, subject_id):
     c = RequestContext(request)
     s = Subject.objects.get(id = subject_id)
-    instructions_list = s.instruction_set.all()[0:5]
-    return render_to_response('ins/subject.html', {'instructions_list': instructions_list})
+    instructions_list = s.instruction_set.all()
+    return render_to_response('ins/subject.html', {'instructions_list': instructions_list},c)
 
+@permission_required_or_403('view_instruction')
 def instruction(request, instruction_id):
     c = RequestContext(request)
     instruction = Instruction.objects.get(id = instruction_id)
-    task_list = instruction.task_set.all()[0:5]
+    task_list = instruction.task_set.all()
     task_list = [ [task, task.snippet_set.all()] for task in task_list]
-    return render_to_response('ins/instruction.html', {'instruction': instruction, 'task_list': task_list})
+    return render_to_response('ins/instruction.html', {'instruction': instruction, 'task_list': task_list},c)
 
 def loginuser(request):
     c = RequestContext(request)
