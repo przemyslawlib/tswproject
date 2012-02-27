5e188ef188bac710adb8a633566aad2c11ed6089
Przemysław Lib <przemyslaw.lib@wp.pl>
2012-02-27 11:38:14 +0100

    Proper order for snippets, tasks, instructions

--- tswproject/ins/views.py
+++ tswproject/ins/views.py
@@ -7,21 +7,21 @@ from guardian.decorators import permission_required_or_403
 # Create your views here.
 def home(request):
     c = RequestContext(request)
-    subject_list = Subject.objects.all()
+    subject_list = Subject.objects.all().order_by('title')
     return render_to_response('ins/index.html', {'subject_list': subject_list}, c)
 
 def subject(request, subject_id):
     c = RequestContext(request)
     s = Subject.objects.get(id = subject_id)
-    instructions_list = s.instruction_set.all()
+    instructions_list = s.instruction_set.all().order_by('title')
     return render_to_response('ins/subject.html', {'instructions_list': instructions_list},c)
 
 @permission_required_or_403('view_instruction')
 def instruction(request, instruction_id):
     c = RequestContext(request)
     instruction = Instruction.objects.get(id = instruction_id)
-    task_list = instruction.task_set.all()
-    task_list = [ [task, task.snippet_set.all()] for task in task_list]
+    task_list = instruction.task_set.all().order_by('title')
+    task_list = [ [task, task.snippet_set.all().order_by("name")] for task in task_list]
     return render_to_response('ins/instruction.html', {'instruction': instruction, 'task_list': task_list},c)
 
 def loginuser(request):
